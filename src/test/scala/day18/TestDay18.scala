d9eb505e3e1350501e054b57cca5894709f2d340
marinos <marinosch.99981@gmail.com>
2024-11-09 16:58:50 +0100

    day 18 unit tests

--- src/test/scala/day18/TestDay18.scala
+++ src/test/scala/day18/TestDay18.scala
@@ -2,9 +2,10 @@ package org.mpdev.scala.aoc2015
 package day18
 
 import framework.InputReader
-import solutions.day18.LightsAnimation
-
+import solutions.day18.{LightState, LightsAnimation}
 import solutions.day18.LightState.ON
+
+import utils.GridBuilder
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers.*
 
@@ -18,15 +19,39 @@ class TestDay18 extends AnyFlatSpec {
         (
             solver.grid.getDimensions, solver.grid.countOf(ON), solver.grid.getDataPoints.size
         ) shouldBe (
-            (6, 6), 15, 15
+            (6, 6), 15, 36
         )
     }
 
+    it should "animate the lights" in {
+        val expectedOutput = List(
+                "......",
+                "......",
+                "..##..",
+                "..##..",
+                "......",
+                "......"
+        )
+        val expectedGid = GridBuilder[LightState]().withMapper(LightState.mapper).fromVisualGrid(expectedOutput).build()
+        println("Initial")
+        var currentGrid = solver.grid
+        currentGrid.printIt()
+        for (count <- 1 to 4) do
+            println(count)
+            currentGrid = solver.animate(currentGrid)
+            currentGrid.printIt()
+        currentGrid.getDataPoints shouldBe expectedGid.getDataPoints
+    }
+
     it should "solve part1 correctly" in {
-        solver.solvePart1(input) shouldBe 101
+        solver.repeatAnimation = 4
+        solver.debug = true
+        solver.solvePart1(input) shouldBe 4
     }
 
     it should "solve part2 correctly" in {
-        solver.solvePart2(input) shouldBe 48
+        solver.repeatAnimation = 5
+        solver.debug = true
+        solver.solvePart2(input) shouldBe 17
     }
 }
